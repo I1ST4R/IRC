4ca5225027bc0fefabdb8756bcd7f8390347c60e
I1ST4R <reshetilov-vanechka@mail.ru>
2025-08-13 11:17:48 +0300

    fix: add tags to cart api for smart refetching

--- src/entity/cart/api.ts
+++ src/entity/cart/api.ts
@@ -13,48 +13,56 @@ import { CartItem } from './types';
 export const cartApi = createApi({
   reducerPath: 'cartApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  tagTypes: ['Cart'],
   endpoints: (build) => ({
     getCart: build.query<CartItem[], string>({
       queryFn: (userId) =>
         getCart(userId)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      providesTags: ['Cart'],
     }),
     addToCart: build.mutation<CartItem[], { userId: string; productId: string }>({
       queryFn: ({ userId, productId }) =>
         addToCart(userId, productId)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      invalidatesTags: ['Cart'],
     }),
     updateCartItemQuantity: build.mutation<CartItem[], { userId: string; productId: string; quantity: number }>({
       queryFn: ({ userId, productId, quantity }) =>
         updateCartItemQuantity(userId, productId, quantity)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      invalidatesTags: ['Cart'],
     }),
     removeFromCart: build.mutation<void, { userId: string; productId: string }>({
       queryFn: ({ userId, productId }) =>
         removeFromCart(userId, productId)
           .then(() => ({ data: undefined }))
           .catch((error) => ({ error })),
+      invalidatesTags: ['Cart'],
     }),
     changeCheckCartItem: build.mutation<CartItem, { userId: string; productId: string }>({
       queryFn: ({ userId, productId }) =>
         changeCheckCartItem(userId, productId)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      invalidatesTags: ['Cart'],
     }),
     clearCart: build.mutation<CartItem[], { userId: string }>({
       queryFn: ({ userId }) =>
         clearCart(userId)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      invalidatesTags: ['Cart'],
     }),
     getCartTotals: build.query<number, string>({
       queryFn: (userId) =>
         calculateCartTotals(userId)
           .then((data) => ({ data }))
           .catch((error) => ({ error })),
+      providesTags: ['Cart'],
     }),
   }),
 });
