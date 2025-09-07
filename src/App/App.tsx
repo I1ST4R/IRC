60662f7b1fc3454712918e7f05ae5bac77fe04ac
I1ST4R <reshetilov-vanechka@mail.ru>
2025-09-07 20:42:56 +0300

    feat: css variables moved to app.css, OrderMenu rewritten on tailwind as tailwind practice

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,81 +1,93 @@
-import React from 'react';
+import React from "react";
 // import { Outlet } from 'react-router-dom';
 // import Header from './components/Header/Header';
 // import Footer from './components/Footer/Footer';
 // import Navbar from './components/Navbar/Navbar';
 // import BottomMenu from './components/BottomMenu/BottomMenu';
 // import PersonalAccount from './components/PersonalAccount/PersonalAccount';
-import './_app.scss';
+import "./app.css";
 
 const App: React.FC = () => {
-
   return (
-    <div className="bg-[#F2F2F2] w-73">
-      <p className="p-3 border-b border-gray-200">Ваш заказ</p>
+    <div className="w-full h-[100vh]">
+      <div className="bg-[#F2F2F2] w-73 pb-6">
+        <p className="p-3 border-b border-gray-200">Ваш заказ</p>
 
-      <div className="order-menu__item">
-        <div className="order-menu__text">
-          <span className="order-menu__label">Товары на сумму:</span>
-          <span className="order-menu__value">{228} ₽</span>
-        </div>
-        <div className="order-menu__text">
-          <span className="order-menu__label">Скидка:</span>
-          <span className="order-menu__value order-menu__value--discount">
-            {69} ₽
-          </span>
-        </div>
-        {true && (
-          <div className="order-menu__text">
-            <span className="order-menu__label">Скидка по промокоду:</span>
-            <span className="order-menu__value order-menu__value--discount">
-              {100} ₽
-            </span>
+        <div
+          className="p-3 border-b border-gray-200 font-['Manrope'] font-bold 
+          text-xs tracking-widest uppercase flex flex-col gap-2.5"
+        >
+          <div className="flex justify-between items-center">
+            <span>Товары на сумму:</span>
+            <span className="text-[var(--coral)]">{228} ₽</span>
           </div>
-        )}
-        {true && (
-          <div className="order-menu__text">
-            <span className="order-menu__label">Скидка по сертификату:</span>
-            <span className="order-menu__value order-menu__value--discount">
-              {200} ₽
-            </span>
+          <div className="flex justify-between items-center">
+            <span>Скидка:</span>
+            <span className="text-[var(--coral)]">{69} ₽</span>
           </div>
-        )}
-        <div className="order-menu__text">
-          <span className="order-menu__label">Доставка:</span>
-          <span className="order-menu__value">{1488} ₽</span>
+          {true && (
+            <div className="flex justify-between items-center">
+              <span>Скидка по промокоду:</span>
+              <span className="text-[var(--coral)]">{100} ₽</span>
+            </div>
+          )}
+          {true && (
+            <div className="flex justify-between items-center">
+              <span>Скидка по сертификату:</span>
+              <span className="text-[var(--coral)]">{200} ₽</span>
+            </div>
+          )}
+          <div className="flex justify-between items-center">
+            <span>Доставка:</span>
+            <span className="text-[var(--coral)]">{1488} ₽</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <span>Всего к оплате:</span>
+            <span className="text-[var(--coral)]">{2564} ₽</span>
+          </div>
         </div>
-        <div className="order-menu__text">
-          <span className="order-menu__label">Всего к оплате:</span>
-          <span className="order-menu__value">{2564} ₽</span>
+
+        <div
+          className="p-3 border-b border-gray-200 font-['Manrope'] 
+          font-bold text-xs tracking-widest uppercase flex flex-col gap-2.5"
+        >
+          {true && (
+            <div className="flex flex-col cursor-pointer">
+              <input
+                type="text"
+                placeholder="Промокод"
+                className="flex-1 p-6 border border-[#e0e0e0] rounded-4xl text-sm 
+                ml-0 mb-2.5 min-w-30 block bg-[rgba(0,0,0,0.05)] font-normal 
+                uppercase text-black outline-0 focus:border-[var(--coral)]
+                focus:outline-none transition-colors"
+              />
+            </div>
+          )}
+          {true && (
+            <div className="flex flex-col cursor-pointer">
+              <input
+                type="text"
+                placeholder="Сертификат"
+                className="flex-1 p-6 border border-[#e0e0e0] rounded-4xl text-sm 
+                ml-0 mb-2.5 min-w-30 block bg-[rgba(0,0,0,0.05)] font-normal uppercase
+              text-black outline-0 focus:border-[var(--coral)] focus:outline-none transition-colors"
+              />
+            </div>
+          )}
         </div>
+
+        <button
+          className="bg-[var(--coral)] text-white font-bold text-xs uppercase flex items-center 
+           justify-center w-[80%] h-16 box-content rounded-4xl m-auto mt-6 transition-colors duration-300
+           hover:bg-[var(--coralDark)] hover:text-black"
+          onClick={() => alert("Заебись")}
+          style={{ fontFamily: "Manrope" }}
+        >
+          Оформить заказ
+        </button>
       </div>
-
-      <div className="order-menu__item">
-        {true && (
-          <div className="order-menu__field">
-            <input
-              type="text"
-              placeholder="Промокод"
-              className={`order-menu__input`}
-            />
-          </div>
-        )}
-        {true && (
-          <div className="order-menu__field">
-            <input
-              type="text"
-              placeholder="Сертификат"
-              className={`order-menu__input`}
-            />
-          </div>
-        )}
-      </div>
-
-      <button className="order-menu__button" onClick={() => alert("Заебись")}>
-        Оформить заказ
-      </button>
     </div>
-  )
+  );
 
   // return (
   //   <div className="app">
