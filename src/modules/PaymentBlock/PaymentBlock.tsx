0c81d23451a86ea89bfc6f0d9d87a8cbd6ac7560
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-10 12:00:51 +0300

    refactor: dekete garbage functions in PaymentBlock

--- src/modules/PaymentBlock/PaymentBlock.tsx
+++ src/modules/PaymentBlock/PaymentBlock.tsx
@@ -10,10 +10,6 @@ import { useNavigate } from "react-router-dom";
 export const PaymentBlock = () => {
   const order = useSelector((state: RootState) => state.orders.current);
   const {data: user} = useGetUserQuery()
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
-  const [phoneError, setPhoneError] = useState("");
-  const [cardError, setCardError] = useState("");
   const navigate = useNavigate();
 
   const [createOrder] = useAddOrderMutation();
@@ -27,63 +23,6 @@ export const PaymentBlock = () => {
       </div>
     );
   }
-
-  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/\D/g, "");
-    if (value === "" || (value.startsWith("7") && value.length <= 11)) {
-      setPhoneNumber(value);
-    }
-  };
-
-  // Обработчик изменения номера карты
-  const handleCardChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.replace(/\D/g, "");
-    if (value.length <= 16) {
-      const formattedValue = value.replace(/(\d{4})(?=\d)/g, "$1 ");
-      setCardNumber(formattedValue);
-    }
-  };
-
-  const validatePhone = () => {
-    if (phoneNumber.length !== 11) {
-      setPhoneError("Номер должен содержать 11 цифр");
-    } else {
-      setPhoneError("");
-    }
-  };
-
-  const validateCard = () => {
-    if (cardNumber.replace(/\s/g, "").length !== 16) {
-      setCardError("Номер карты должен содержать 16 цифр");
-    } else {
-      setCardError("");
-    }
-  };
-
-  const removeCartItems = async () => {
-    const cartItemsId = order.cartItems.map((cartItem: CartItem) => {
-      return cartItem.product.id
-    })
-    for (const cartItemId of cartItemsId) {
-      await removeFromCart({userId: user?.id! ,productId: cartItemId});
-    }
-  }
-
-  const handlePaymentCreate = () => {
-    if (order.recipient.paymentMethod === "SBP") {
-      validatePhone();
-    } else validateCard();
-
-
-    //() => navigate("/cart")
-    if (!phoneError && !cardError) {
-      createOrder(order).then(() => {
-        removeCartItems().then(() => navigate("/cart"))
-      })
-      setPhoneError("");
-      setCardError("");
-    }
-  };
 
   return (
     <div className="payment container">
