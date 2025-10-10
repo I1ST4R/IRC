de2fdff7bfdde179a055f6ddf0f9590dce39e077
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-10 11:07:17 +0300

    feat: create formConfig for PaymentBlock

--- src/modules/PaymentBlock/formConfig.ts
+++ src/modules/PaymentBlock/formConfig.ts
@@ -1,3 +1,5 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 import z from "zod";
 
 const SBPPaymentSchema = z.object({
@@ -7,6 +9,28 @@ const SBPPaymentSchema = z.object({
   .regex(/^[\d\+\-\(\)\s]+$/, "Номер телефона содержит недопустимые символы") 
 })
 
+export type SBPPaymentData = z.infer<typeof SBPPaymentSchema>
+
+export const SBPPaymentForm = useForm<SBPPaymentData>({
+  resolver: zodResolver(SBPPaymentSchema),
+  defaultValues: {
+    phone: ""
+  },
+  mode: "onChange"
+})
+
+
+
 const cardPaymentSchema = z.object({
   number: z.string().regex(/^\d+$/, "Номер банковской карты должен содержать только цифры")
+})
+
+export type cardPaymentData = z.infer<typeof cardPaymentSchema>
+
+export const cardPaymentForm = useForm<cardPaymentData>({
+  resolver: zodResolver(cardPaymentSchema),
+  defaultValues: {
+    number: ""
+  },
+  mode: "onChange"
 })
