9d9538cb6e0f664243e2e4a0c295e4b80b95eb66
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-10 21:37:22 +0300

    feat: add shadcn rhf form to SBPPaymentForm

--- src/modules/PaymentBlock/components/SBPPaymentForm/SBPPaymentForm.tsx
+++ src/modules/PaymentBlock/components/SBPPaymentForm/SBPPaymentForm.tsx
@@ -1,3 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import { onSBPPaymentSubmit, SBPPaymentData, SBPPaymentForm as SBPPaymentFormConfig } from "./SBPPaymentFormConfig";
+import { useAppDispatch } from "@/modules/OrderMenu";
+import { useGetUserQuery } from "@/shared/store/user/userApiSlice";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shared/ui/kit/form";
+import { Input } from "@/shared/ui/kit/input";
+import { Button } from "@/shared/ui/kit/button";
+import { ControllerRenderProps } from "react-hook-form";
+
 export const SBPPaymentForm = () => {
-  
-}
+  const { control } = SBPPaymentFormConfig
+
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const {data: user} = useGetUserQuery()
+
+  return (
+    <Form {...SBPPaymentFormConfig}>
+      <form
+        onSubmit={() => onSBPPaymentSubmit(dispatch, navigate, user?.id ?? "")}
+        className="space-y-6"
+      >
+        <FormField
+          control={control}
+          name="phone"
+          render={({ field }: { field: ControllerRenderProps<SBPPaymentData, "phone"> }) => (
+            <FormItem>
+              <FormLabel>Телефон</FormLabel>
+              <FormControl>
+                <Input placeholder="телефон" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="w-full">
+          Оплатить
+        </Button>
+      </form>
+    </Form>
+  );
+};
