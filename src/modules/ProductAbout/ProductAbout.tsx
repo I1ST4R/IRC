e6b80f77c9a723744275475f7bef620da2a922cf
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-11 22:21:04 +0300

    feat: create ProductLikeBtn

--- src/modules/ProductAbout/ProductAbout.tsx
+++ src/modules/ProductAbout/ProductAbout.tsx
@@ -1,13 +1,12 @@
 import { useParams, useNavigate, Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
 import { BreadCrumb } from "@/shared/ui/components/BreadCrumb";
 import { useGetUserQuery } from "@/shared/store/user/userApiSlice";
-import { useGetProductByIdQuery } from "../ProductList";
-import { useAddToCartMutation, useGetCartQuery } from "../CartBody";
+import { Tag, useGetProductByIdQuery } from "../ProductList";
 import { Unauthorized } from "@/shared/ui/components/Unauthorized";
 import { openAccount, useAppDispatch } from "../AuthForm";
 import { ProductPrices } from "./components/ProductPrices";
 import { ProductCartBtn } from "./components/ProductCartBtn";
+import { ProductLikeBtn } from "./components/ProductLikeBtn";
 
 export const ProductAbout = () => {
   const { id } = useParams();
@@ -16,7 +15,6 @@ export const ProductAbout = () => {
     skip: !id,
   });
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   if (error) return <div>Ошибка при загрузке продукта</div>;
   if (!product) return <div>Товар не найден</div>;
@@ -60,29 +58,7 @@ export const ProductAbout = () => {
               prevPrice={product.prevPrice}
             />
             <ProductCartBtn userId={user.id} productId={product.id} />
-
-            <button
-              className={`product__like product-about__like ${
-                isLiked() ? " product__like--active" : ""
-              }`}
-              onClick={() => switchLike(product.id)}
-              title={isLiked() ? "Убрать из избранного" : "В избранное"}
-            >
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-              >
-                <path
-                  d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                  fill={isLiked() ? "#CA354F" : "#ECECEC"}
-                  stroke="#CA354F"
-                  strokeWidth="1.5"
-                />
-              </svg>
-            </button>
+            <ProductLikeBtn userId={user.id} productId={product.id}/>
           </div>
 
           <div className="product-about__point product-about__point--first">
