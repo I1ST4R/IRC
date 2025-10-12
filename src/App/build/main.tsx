81650eba5783e246c230611830b07732a8abc1af
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-12 13:25:40 +0300

    fix: fix router in main.tsx

--- src/App/build/main.tsx
+++ src/App/build/main.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './main/styles/main.scss';
 import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
-// import { store } from './main/store';
-import { router } from '../routes';
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'; // ← добавляем
+// import { store } from './path-to-your-store';
+import './index.css';
 import App from '../App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider> */}
-    <App/>
+    <Provider store={store}>
+      <BrowserRouter> 
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
