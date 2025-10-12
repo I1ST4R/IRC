d64cd0a108912d7aa3d23260310974aee8551405
I1ST4R <reshetilov-vanechka@mail.ru>
2025-10-12 10:12:52 +0300

    fix: fix App component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,29 +1,27 @@
 import React from "react";
-// import { Outlet } from 'react-router-dom';
-// import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
-// import Navbar from './components/Navbar/Navbar';
-// import BottomMenu from './components/BottomMenu/BottomMenu';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import BottomMenu from './components/BottomMenu/BottomMenu';
 import "./app.css";
 import { AuthForm } from "@/modules/AuthForm/AuthForm";
+import Navbar from "./components/Navbar";
 
 const App: React.FC = () => {
-  return (
-    <AuthForm/>
-  );
 
-  // return (
-  //   <div className="app">
-  //     <Header />
-  //     <Navbar />
-  //     <main className="app__content">
-  //       <Outlet />
-  //       <PersonalAccount />
-  //     </main>
-  //     <Footer />
-  //     <BottomMenu />
-  //   </div>
-  // );
+  return (
+    <div className="app">
+      <Header />
+      <Navbar />
+      <main className="app__content">
+        <Outlet />
+        <AuthForm/>
+      </main>
+      <Footer />
+      <BottomMenu />
+    </div>
+  );
 };
 
 export default App;
